e90255bde09c5647c7b6ba38969f7f2fb636ca64
MuthuKrishnan27 <muthukrishnan.office@gmail.com>
2023-07-28 11:11:17 +0530

    submitting code-Fri Jul 28 2023 11:11:15 GMT+0530 (India Standard Time)

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,7 @@ const Categories = ({ categories, filterItems }) => {
   return (
     <div className="categories">
       {categories.map((category, index) => (
-        <button id='filter-btn-{index}' key={index} onClick={() => filterItems(category)}>
+        <button  id={`button-filter-${index}`} key={index} onClick={() => filterItems(category)}>
           {category}
         </button>
       ))}
